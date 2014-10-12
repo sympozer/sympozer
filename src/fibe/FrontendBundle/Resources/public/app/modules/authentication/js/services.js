1aaaab1a1045f545a642d3c784c4e5b5817ccfe9
Benoit <benoitddlp@gmail.com>
2014-10-12 23:54:28 +0200

    first push

---
+++ src/fibe/FrontendBundle/Resources/public/app/modules/authentication/js/services.js
@@ -0,0 +1,32 @@
+/**
+ * User Factory
+ *
+ * Service calls for following actions :
+ * - get
+ * - signin
+ * - signup
+ * - show
+ * - list
+ *
+ * @type {factory}
+ */
+angular.module('authenticationApp').factory('usersFact',
+    ['$resource', function ($resource)
+    {
+        return $resource(
+            globalConfig.api.urls.login,
+            {id: '@id'},
+            {
+                get            : {method: 'GET', params: {}, isArray: false},
+                signin         : {method: 'POST', params: {}, isArray: false},
+                signup         : {method: 'POST', params: {no_clean: true}, url: globalConfig.api.urls.registration, isArray: false},
+                signout        : {method: 'GET', url: globalConfig.api.urls.logout, isArray: false},
+                confirm        : {method: 'POST', url: globalConfig.api.urls.confirm, isArray: false},
+                changepwd      : {method: 'POST', url: globalConfig.api.urls.changepwd, isArray: false},
+                resetpwdrequest: {method: 'POST', url: globalConfig.api.urls.reset_pwd_request, isArray: false},
+                resetpwd       : {method: 'POST', url: globalConfig.api.urls.reset_pwd, isArray: false},
+                show           : {method: 'GET', isArray: false}
+            }
+        );
+    }]);
+
