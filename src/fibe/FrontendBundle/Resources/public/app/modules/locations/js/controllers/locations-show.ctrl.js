09af7bb3c66577d0cedc99b7b8d5b3b7684d037a
flepeutr <flepeutrec@gmail.com>
2014-10-19 18:12:33 +0200

    Show orgnization/paper/location

--- src/fibe/FrontendBundle/Resources/public/app/modules/locations/js/controllers/locations-show.ctrl.js
+++ src/fibe/FrontendBundle/Resources/public/app/modules/locations/js/controllers/locations-show.ctrl.js
@@ -6,5 +6,14 @@
 angular.module('locationsApp').controller('locationsShowCtrl', [ '$scope', '$routeParams', 'locationsFact', function ($scope, $routeParams, locationsFact)
 {
     $scope.location = locationsFact.get({id: $routeParams.locationId});
+     //initialize map zoom
+    $scope.center = { zoom: 2 }
+    $scope.location = locationsFact.get({id: $routeParams.locationId}, function(location){
+        //initialize map with current location
+        $scope.center.lat = location.latitude || 48;
+        $scope.center.long = location.longitude  || 8;
+        $scope.center.zoom = 4;
+        $scope.markers.push($scope.center);
+    });
 
 }]);
