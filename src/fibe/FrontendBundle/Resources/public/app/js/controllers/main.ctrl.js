ced681e23c18fe7f3ccac661d52d13161af8d4a4
florian.bacle <bacle.florian@gmail.com>
2014-10-21 11:17:25 +0200

    fix select2 templates urls

--- src/fibe/FrontendBundle/Resources/public/app/js/controllers/main.ctrl.js
+++ src/fibe/FrontendBundle/Resources/public/app/js/controllers/main.ctrl.js
@@ -4,11 +4,15 @@
  * @type {controller}
  */
 angular.module('sympozerApp').controller('mainCtrl',
-    ['$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG',
-        function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG)
+    ['$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG', 'uiSelectConfig',
+        function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG, uiSelectConfig)
         {
             $scope.GLOBAL_CONFIG = GLOBAL_CONFIG;
             $rootScope.GLOBAL_CONFIG = GLOBAL_CONFIG;
+
+            debugger;
+            uiSelectConfig.theme = GLOBAL_CONFIG.app.urls.partials +'select2/';
+
             $scope.scrollTop = function ()
             {
                 $('html, body').animate({scrollTop: 0}, 'slow');
