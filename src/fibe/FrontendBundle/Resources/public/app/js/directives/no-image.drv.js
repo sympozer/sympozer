35784233e96f4591992297108ec26612a434d29e
florian.bacle <bacle.florian@gmail.com>
2014-10-21 12:22:44 +0200

    fix on image rendering, ng-img instead of img

---
+++ src/fibe/FrontendBundle/Resources/public/app/js/directives/no-image.drv.js
@@ -0,0 +1,31 @@
+/**
+ * Directive for image replacement when image is not available
+ * @see http://alex-taran.blogspot.com/2013/10/angular-js-no-image-directive.html
+ * use it like :
+ *
+ *  <img no-image ng-src="{product.imageUrl}}" />
+ *
+ */
+angular.module('sympozerApp').directive('noImage', ['$rootScope', function ($rootScope) {
+
+    var setDefaultImage = function (el) {
+        el.attr('src', $rootScope.GLOBAL_CONFIG.app.urls.img+'no-picture.jpg');
+    };
+
+    return {
+        restrict: 'A',
+        link: function (scope, el, attr) {
+            scope.$watch(function() {
+                return attr.ngSrc;
+            }, function () {
+                var src = attr.ngSrc;
+
+                if (!src) {
+                    setDefaultImage(el);
+                }
+            });
+
+            el.bind('error', function() { setDefaultImage(el); });
+        }
+    };
+}]);
