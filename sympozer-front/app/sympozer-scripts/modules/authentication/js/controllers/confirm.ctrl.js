c3a9daba6ea371f570d77c6269adc5af06f21b59
Benoit <benoitddlp@gmail.com>
2014-11-16 23:44:47 +0100

    signup action

--- sympozer-front/app/sympozer-scripts/modules/authentication/js/controllers/confirm.ctrl.js
+++ sympozer-front/app/sympozer-scripts/modules/authentication/js/controllers/confirm.ctrl.js
@@ -5,7 +5,7 @@
  * @type {controller}
  */
 angular.module('authenticationApp').controller('confirmCtrl',
-    [ '$scope', '$rootScope', '$routeParams', 'usersFact', '$location', function ($scope, $rootScope, $routeParams, usersFact, $location)
+    [ '$scope', '$rootScope', '$routeParams', 'usersFact', 'pinesNotifications', 'translateFilter', 'authenticationFact', '$location', function ($scope, $rootScope, $routeParams, usersFact, pinesNotifications, translateFilter, authenticationFact, $location)
     {
         var error = function (response, args)
         {
@@ -24,6 +24,23 @@ angular.module('authenticationApp').controller('confirmCtrl',
             $rootScope.$broadcast('AlertCtrl:addAlert', {code: 'Register_confirm_success', type: 'success'});
             $location.path('/profile');
         }
+
+        var success = function (user)
+        {
+            $scope.user = user;
+
+            //Modify current user
+            authenticationFact.addUser(user);
+
+            //Notify of the signin action success
+            pinesNotifications.notify({
+                title: translateFilter('global.validations.success'),
+                text : translateFilter('authentication.validations.signin_success'),
+                type : 'success'
+            });
+
+            $location.path('/home/persons/show/' + user.person.id);
+        };
 
         $scope.busy = true;
         var user = usersFact.confirm({token: $routeParams.token}, success, error);
