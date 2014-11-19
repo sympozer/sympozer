42faae2dc1d8a9b024965628f6cac178ca471288
Benoit <benoitddlp@gmail.com>
2014-11-20 00:40:05 +0100

    frontend : teammate : add template

--- sympozer-front/app/sympozer-scripts/modules/teammates/js/routes.js
+++ sympozer-front/app/sympozer-scripts/modules/teammates/js/routes.js
@@ -14,6 +14,10 @@ angular.module('teammatesApp')
                     templateUrl: globalConfig.app.modules.teammates.urls.partials + 'teammates-index.html',
                     controller : 'teammatesIndexCtrl'
                 })
+                .when('/teammates/new', {
+                    templateUrl: globalConfig.app.modules.teammates.urls.partials + 'teammates-new.html',
+                    controller : 'teammatesNewCtrl'
+                })
 //                .when('/teammates/list', {
 //                    templateUrl: globalConfig.app.modules.teammates.urls.communityPartials + 'teammates-community-thumbnail.html',
 //                    controller : 'teammatesCommunityListCtrl'
