06f488c278be3ef8b79add0a03f17ca0fc0f2c70
florian.bacle <bacle.florian@gmail.com>
2014-11-23 06:46:52 +0100

    0000201: [V2][Theme][Frontend] Restore main event module - logo change management

--- sympozer-front/app/sympozer-scripts/modules/main-events/js/controllers/main-events-edit.ctrl.js
+++ sympozer-front/app/sympozer-scripts/modules/main-events/js/controllers/main-events-edit.ctrl.js
@@ -5,121 +5,144 @@
  */
 angular.module('mainEventsApp').controller('mainEventsEditCtrl', [ '$scope', '$rootScope', '$routeParams', '$location', 'mainEventsFact', 'pinesNotifications', 'translateFilter', '$modal', 'GLOBAL_CONFIG', function ($scope, $rootScope, $routeParams, $location, mainEventsFact, pinesNotifications, translateFilter, $modal, GLOBAL_CONFIG)
 {
-    var fetchSuccess = function(mainEvent){
-        geoCode(mainEvent.location.label);
-    }
+    //Execute only if not in a modal context
+    if(!$scope.$close){
 
-    $scope.mainEvent = mainEventsFact.get({id: $routeParams.mainEventId}, fetchSuccess);
+        //Fetch the mainEvent
+        $scope.mainEvent = mainEventsFact.get({id: $routeParams.mainEventId}, fetchSuccess);
 
-    //Mandatory for the map plugin gmap to work
-    $scope.geoCodingMapInstance;
-
-    //Set default options for map rendering (mandatory)
-    $scope.mapInstanceOption = {
-        lat: -12.043333,
-        lng: -77.028333,
-        zoom: 12
-    }
-
-    //On address defintion
-    $scope.setAddress = function(selectedAddress){
-        //trigger map rendering of the selected address
-        geoCode(selectedAddress.label);
-        //Set new conference address
-        $scope.mainEvent.location = selectedAddress;
-    }
+        var fetchSuccess = function(mainEvent){
+            geoCode(mainEvent.location.label);
+        }
 
 
+        //Mandatory for the map plugin gmap to work
+        $scope.geoCodingMapInstance;
 
-    var error = function (response, args)
-    {
-        //Notify of the field update action error
-        pinesNotifications.notify({
-            title: translateFilter('global.validations.error'),
-            text: translateFilter('response.data.error.message'),
-            type: 'error'
-        });
-    };
+        //Set default options for map rendering (mandatory)
+        $scope.mapInstanceOption = {
+            lat: -12.043333,
+            lng: -77.028333,
+            zoom: 12
+        }
 
-    var success = function (response, args)
-    {
-        //Notify of the field update action success
-        pinesNotifications.notify({
-            title: translateFilter('global.validations.success'),
-            text: translateFilter('global.validations.modifications_saved'),
-            type: 'success'
-        });
-    };
+        //On address defintion
+        $scope.setAddress = function(selectedAddress){
+            //trigger map rendering of the selected address
+            geoCode(selectedAddress.label);
+            //Set new conference address
+            $scope.mainEvent.location = selectedAddress;
+        }
 
-    var geoCode = function(address){
-        GMaps.geocode({
-            address: address,
-            callback: function (results, status)
-            {
-                if (status == 'OK')
+
+        //On update main event error
+        var error = function (response, args)
+        {
+            //Notify of the field update action error
+            pinesNotifications.notify({
+                title: translateFilter('global.validations.error'),
+                text: translateFilter('response.data.error.message'),
+                type: 'error'
+            });
+        };
+
+        //On update main event success
+        var success = function (response, args)
+        {
+            //Notify of the field update action success
+            pinesNotifications.notify({
+                title: translateFilter('global.validations.success'),
+                text: translateFilter('global.validations.modifications_saved'),
+                type: 'success'
+            });
+        };
+
+        /**
+         * Set the map with a marker at a specific address
+         * @param address, address to geocode and put the marker on
+         */
+        var geoCode = function(address){
+            GMaps.geocode({
+                address: address,
+                callback: function (results, status)
                 {
-                    var latlng = results[0].geometry.location;
-                    $scope.geoCodingMapInstance.setCenter(latlng.lat(), latlng.lng());
-                    $scope.geoCodingMapInstance.addMarker({
-                        lat: latlng.lat(),
-                        lng: latlng.lng()
-                    });
+                    if (status == 'OK')
+                    {
+                        var latlng = results[0].geometry.location;
+                        //Set map center
+                        $scope.geoCodingMapInstance.setCenter(latlng.lat(), latlng.lng());
+                        //Set marker
+                        $scope.geoCodingMapInstance.addMarker({
+                            lat: latlng.lat(),
+                            lng: latlng.lng()
+                        });
+                    }
                 }
+            });
+        }
+
+        //Context change
+        $rootScope.$broadcast('contextCtrl:changeContext', {mainEventId: $routeParams.mainEventId});
+
+
+        //New start date validation
+        $scope.onSelectStart = function(newDate){
+
+            //Verify startAt =< endAt
+            if (moment(newDate).isBefore(moment($scope.mainEvent.endAt)) || moment(newDate).isSame(moment($scope.mainEvent.endAt)))
+            {
+                $scope.updateMainEvent('startAt', $scope.mainEvent.startAt);
+            }
+        }
+
+        //New end date validation
+        $scope.onSelectEnd = function(newDate){
+            //Verify startAt < endAt
+            if (moment(newDate).isAfter(moment($scope.mainEvent.startAt)) || moment(newDate).isSame(moment($scope.mainEvent.startAt)))
+            {
+                $scope.updateMainEvent('endAt', $scope.mainEvent.endAt);
+            }
+        }
+
+
+        //Bind new map instance from plugin to scope
+        $scope.$on('GMaps:created', function (event, mapInstance) {
+            if (mapInstance.key) {
+                $scope[mapInstance.key] = mapInstance.map;
             }
         });
     }
 
+    //Send patch request to server to update a given field
     $scope.updateMainEvent = function(field, data){
+        debugger;
         var updateMainEvent = {id: $scope.mainEvent.id};
         updateMainEvent[field] = data;
         return mainEventsFact.patch(updateMainEvent, success, error);
     };
 
 
-    //Context change
-    $rootScope.$broadcast('contextCtrl:changeContext', {mainEventId: $routeParams.mainEventId});
-
-
-    //New start date validation
-    $scope.onSelectStart = function(newDate){
-
-        //Verify startAt =< endAt
-        if (moment(newDate).isBefore(moment($scope.mainEvent.endAt)) || moment(newDate).isSame(moment($scope.mainEvent.endAt)))
-        {
-            $scope.updateMainEvent('startAt', $scope.mainEvent.startAt);
-        }
-    }
-
-    //New end date validation
-    $scope.onSelectEnd = function(newDate){
-        //Verify startAt < endAt
-        if (moment(newDate).isAfter(moment($scope.mainEvent.startAt)) || moment(newDate).isSame(moment($scope.mainEvent.startAt)))
-        {
-            $scope.updateMainEvent('endAt', $scope.mainEvent.endAt);
-        }
-    }
-
-
-    //Bind new map instance from plugin to scope
-    $scope.$on('GMaps:created', function (event, mapInstance) {
-        if (mapInstance.key) {
-            $scope[mapInstance.key] = mapInstance.map;
-        }
-    });
-
-
+    //Select logo modal workflow
     $scope.showLogoModal = function(){
-        // Open modal with new organization form
+        // Open modal with main event logo form
         var modalInstance = $modal.open({
             templateUrl: GLOBAL_CONFIG.app.modules.mainEvents.urls.partials + 'main-events-select-logo.html',
+            //The edit controller is responsible for it
             controller: 'mainEventsEditCtrl',
-            size: "large"
+            size: "large",
+            resolve : {
+                //Passing current mainEvent as a parameter
+                mainEvent :function() {
+                    return $scope.mainEvent;
+                }
+            }
         });
-    }
 
-    $scope.setLogo = function(url){
-        $scope.$close();
-        $scope.updateMainEvent('logo', url);
+        //On success modal $close function call, resolve the promise
+        modalInstance.result.then(function (logoUrl) {
+            $scope.mainEvent.logo = logoUrl;
+            $scope.updateMainEvent('logo', logoUrl);
+        })
     }
 
 }]);
