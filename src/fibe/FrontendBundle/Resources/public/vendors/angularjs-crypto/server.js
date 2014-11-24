3ae760a7aa25cde0643ed5841690dc57edb511f0
florian.bacle <bacle.florian@gmail.com>
2014-11-24 05:40:44 +0100

    fixed gitignore problem

--- src/fibe/FrontendBundle/Resources/public/vendors/angularjs-crypto/server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var port = Number(process.env.PORT || 9000);
-connect().
-    use(serveStatic(__dirname+'/public')).
-    listen(port);
