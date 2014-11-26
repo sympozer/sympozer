945530388cc0c188c1a8a046d805bef18d06545a
Benoit <benoitddlp@gmail.com>
2014-11-27 00:18:48 +0100

    starting linked entity persist test

--- reset.sh
+++ reset.sh
@@ -1,7 +1,6 @@
 php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
 php app/console doctrine:schema:update --force
-php app/console assets:install web --symlink
 php app/console sympozer:database:init
 php app/console sympozer:admin:create admin admin@admin.fr admin
 php app/console sympozer:admin:create admin2 admin2@admin.fr admin2
