e166289bf0aab7847c5485114b1a15ed2231b689
brice_buffa <brice.buffa@gmail.com>
2014-11-26 12:54:42 +0100

    nothing really

--- src/fibe/FrontendBundle/Resources/public/app/modules/twitterTimeline/js/controllers/twittertimeline.ctrl.js
+++ src/fibe/FrontendBundle/Resources/public/app/modules/twitterTimeline/js/controllers/twittertimeline.ctrl.js
@@ -10,10 +10,17 @@ angular.module('twitterApp').controller('twitterTimelineCtrl',
 
     $scope.info = GLOBAL_CONFIG.app.modules.socials.getInfo(GLOBAL_CONFIG.app.modules.socials.mapping, "person");
 
-    console.log($scope.info);
+    /*console.log($scope.info);
     console.log($scope[$scope.info.varName]);
-    console.log( ($scope[$scope.info.varName])["dType"] );
-    $scope.ret = $scope[$scope.info.varName];
+    console.log( ($scope[$scope.info.varName])["firstName"] );*/
+
+    console.log("console.log($scope.person);");
+    var ret1 = $scope.person;
+    console.log(ret1);
+    console.log("console.log($scope.person.id);");
+    var ret2 = $scope.person.id;
+    console.log(ret2);
+
 
     $scope.tweets = twitterFact.get({tag: "test"}); //array of tweets
 }]);
