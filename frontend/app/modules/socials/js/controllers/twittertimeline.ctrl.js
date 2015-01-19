988ac28ee8e541e5cec8c09ac68f1cf498fe4154
Benoit <benoitddlp@gmail.com>
2015-01-20 00:11:34 +0100

    twitter timeline on person merge

--- frontend/app/modules/socials/js/controllers/twittertimeline.ctrl.js
+++ frontend/app/modules/socials/js/controllers/twittertimeline.ctrl.js
@@ -3,34 +3,34 @@
  *
  * @type {controller}
  */
-angular.module('socialsApp').controller('twitterTimelineCtrl',
-    [ '$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG', 'twitterFact', '$location',
-      function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG, twitterFact, $location)
-      {
-          // Retrieve the url and get the specific scope
-          //TODO change this !
-          // => use angular's scope inheritance properties or $routeParams or something more appropriated
-          $scope.entitiesLbl = $location.$$path.split('/')[2];
+angular.module('socialsApp').controller('twitterTimelineCtrl', [
+    '$scope', '$rootScope', '$routeParams', 'GLOBAL_CONFIG', 'twitterFact', '$location',
+    function ($scope, $rootScope, $routeParams, GLOBAL_CONFIG, twitterFact, $location)
+    {
+        // Retrieve the url and get the specific scope
+        //TODO change this !
+        // => use angular's scope inheritance properties or $routeParams or something more appropriated
+        $scope.entitiesLbl = $location.$$path.split('/')[2];
 
-          if ($scope.entitiesLbl == "mainEvents" && $scope.$root.currentMainEvent.twitter)
-          {
-              $scope.tweets = twitterFact.getHashTag({tag: $scope.currentMainEvent.twitter}, function success(data)
-              {
-                  $scope.tweets = data.statuses;
-              });
-          }
-          else if ($scope.entitiesLbl == "persons")
-          {
 
-              $scope.$watch('$parent.person', function (person)
-              {
-                  if (person && person.twitter)
-                  {
-                      $scope.tweets = twitterFact.getPersonTag({tag: person.twitter }, function success(data)
-                      {
-                          $scope.tweets = data;
-                      });
-                  }
-              }, true);
-          }
-      }]);
+        if ($scope.entitiesLbl == "mainEvents" && $scope.$root.currentMainEvent.twitter)
+        {
+            $scope.tweets = twitterFact.getHashTag({tag: $scope.currentMainEvent.twitter}, function success(data)
+            {
+                $scope.tweets = data.statuses;
+            });
+        }
+        else if ($scope.entitiesLbl == "persons")
+        {
+            $scope.$watch('$parent.person', function (person)
+            {
+                if (person && person.twitter)
+                {
+                    $scope.tweets = twitterFact.getPersonTag({tag: person.twitter }, function success(data)
+                    {
+                        $scope.tweets = data;
+                    });
+                }
+            }, true);
+        }
+    }]);
