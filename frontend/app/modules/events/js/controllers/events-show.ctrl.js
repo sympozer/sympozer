996a3c28497d1aec498291f0cde5e3c22d99d49d
florian.bacle <bacle.florian@gmail.com>
2015-02-25 08:45:52 +0100

    Fix on event show page grapgical tweak

--- frontend/app/modules/events/js/controllers/events-show.ctrl.js
+++ frontend/app/modules/events/js/controllers/events-show.ctrl.js
@@ -3,8 +3,16 @@
  *
  * @type {controller}
  */
-angular.module('eventsApp').controller('eventsShowCtrl', [ '$scope', '$routeParams', 'eventsFact', function ($scope, $routeParams, eventsFact)
+angular.module('eventsApp').controller('eventsShowCtrl', [ '$scope', '$routeParams', 'eventsFact', 'moment', function ($scope, $routeParams, eventsFact, moment)
 {
-    $scope.event = eventsFact.get({id: $routeParams.eventId});
+    $scope.event = eventsFact.get({id: $routeParams.eventId}, function(){
+        if($scope.event.startAt && $scope.event.location){
+            //Calculate difference between start and end
+            var eventDuration = moment.duration(new moment($scope.event.endAt) - new moment($scope.event.startAt));
+            //Create an understanable string to express the duration
+            $scope.event.duration = eventDuration.humanize();
+        }
+    });
+
 
 }]);
