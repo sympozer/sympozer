049f360e45de9144b68f0333fcaafdc2d89d42cd
florian.bacle <bacle.florian@gmail.com>
2015-02-25 07:30:16 +0100

    Fix on person show role list and page title

--- frontend/app/modules/persons/js/controllers/persons-show.ctrl.js
+++ frontend/app/modules/persons/js/controllers/persons-show.ctrl.js
@@ -3,9 +3,17 @@
  *
  * @type {controller}
  */
-angular.module('personsApp').controller('personsShowCtrl', [ '$scope', '$rootScope', '$routeParams', 'personsFact', function ($scope, $rootScope, $routeParams, personsFact )
+angular.module('personsApp').controller('personsShowCtrl', [ '$scope', '$rootScope', '$routeParams', 'personsFact', 'rolesFact', function ($scope, $rootScope, $routeParams, personsFact, rolesFact )
 {
 
     $scope.person = personsFact.get({id: $routeParams.personId});
+
+    rolesFact.all({'filters[personId]': $routeParams.personId}, function(response){
+        $scope.roles = response.results;
+        $scope.rolesLoaded = true;
+    }, function(){
+        $scope.rolesLoaded = true;
+    });
+
 
 }]);
